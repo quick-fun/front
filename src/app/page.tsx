0e32fe9c56254767421337290b6e8b1a75f47055
Yujin Cha <youjincha4821@gmail.com>
2024-01-15 23:51:16 +0900

    global.css 수정, PollBox 컴포넌트 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p>Main Page</p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"></div>
-      </div>
+    <main className="main-container mt-16">
+      <section>🔥 실시간 Hot</section>
+      <section>😎 최신 투표</section>
     </main>
   );
 }
