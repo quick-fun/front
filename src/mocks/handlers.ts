d9978754dbe0c74c62b6947590cbf7cc48cd1a0e
Yujin Cha <youjincha4821@gmail.com>
2024-01-31 01:11:55 +0900

    fix: handler 함수 GET test용 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,105 @@
 import { http, HttpResponse } from "msw";
 
 export const handlers = [
-  http.get("/user", ({ request }) => {
-    return HttpResponse.json({ name: "John" });
+  http.get("/user", ({ request, params }) => {
+    return HttpResponse.json({
+      data: [
+        {
+          votePostId: 1,
+          title: "제목입니다 제목입니다~~ 제목 테스트",
+          content: "오늘 저녁 뭐 먹을지 고민임",
+          voteItems: [
+            {
+              voteItemId: 1,
+              voteItemTitle: "순두부찌개",
+              voteRate: 0,
+            },
+            {
+              voteItemId: 2,
+              voteItemTitle: "김치찌개",
+              voteRate: 100,
+            },
+          ],
+          tag: {
+            tagId: 1,
+            tagTitle: "태그명",
+          },
+          labels: [
+            {
+              labelId: 1,
+              labelTitle: "라벨명",
+            },
+          ],
+          createdAt: "2024-01-21T09:56:51.849339",
+          commentTotalCount: 0,
+        },
+        {
+          votePostId: 2,
+          title: "두번째 제목입니다 제목입니다~~ 제목 테스트",
+          content: "오늘 저녁 뭐 먹을지 고민임",
+          voteItems: [
+            {
+              voteItemId: 1,
+              voteItemTitle: "순두부찌개",
+              voteRate: 0,
+            },
+            {
+              voteItemId: 2,
+              voteItemTitle: "김치찌개",
+              voteRate: 100,
+            },
+          ],
+          tag: {
+            tagId: 1,
+            tagTitle: "태그명",
+          },
+          labels: [
+            {
+              labelId: 1,
+              labelTitle: "라벨명",
+            },
+          ],
+          createdAt: "2024-01-21T09:56:51.849339",
+          commentTotalCount: 10,
+        },
+        {
+          votePostId: 3,
+          title: "이것은 세번째 제목입니다 제목입니다~~ 제목 테스트",
+          content: "오늘 저녁 뭐 먹을지 고민임 ㄹㅇㅋㅋ",
+          voteItems: [
+            {
+              voteItemId: 1,
+              voteItemTitle: "순두부찌개",
+              voteRate: 0,
+            },
+            {
+              voteItemId: 2,
+              voteItemTitle: "김치찌개",
+              voteRate: 100,
+            },
+          ],
+          tag: {
+            tagId: 1,
+            tagTitle: "태그명",
+          },
+          labels: [
+            {
+              labelId: 1,
+              labelTitle: "라벨명",
+            },
+          ],
+          createdAt: "2024-01-21T09:56:51.849339",
+          commentTotalCount: 1,
+        },
+      ],
+    });
+  }),
+  http.post("/posts", async ({ request }) => {
+    // Read the intercepted request body as JSON.
+    const newPost = await request.json();
+
+    // Don't forget to declare a semantic "201 Created"
+    // response and send back the newly created post!
+    return HttpResponse.json(newPost, { status: 201 });
   }),
 ];
