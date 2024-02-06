66ba0740dd21d959634cbc5fbd2211b1e2aeaae1
Yujin Cha <youjincha4821@gmail.com>
2024-02-06 23:40:54 +0900

    add: temp 카카오 로그인 인가 코드 요청

--- src/app/@modal/(.)login/page.tsx
+++ src/app/@modal/(.)login/page.tsx
@@ -2,6 +2,7 @@
 
 import Modal from "@/app/ui/common/Modal";
 import Image from "next/image";
+import Link from "next/link";
 
 const LoginModal = () => {
   const fetchLogin = async () => {
@@ -15,6 +16,8 @@ const LoginModal = () => {
     console.log(parsedData);
   };
 
+  const REDIRECT_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}`;
+
   return (
     <Modal>
       <div className="mt-6 flex h-full flex-col justify-between">
@@ -23,14 +26,14 @@ const LoginModal = () => {
           <br />
           간편하게 투표를 만들어보세요!
         </h3>
-        <button onClick={fetchLogin}>
+        <Link href={REDIRECT_URL}>
           <Image
             src="/images/kakao_login_btn.png"
             alt="Kakao login button image"
             width="300"
             height="200"
           />
-        </button>
+        </Link>
       </div>
     </Modal>
   );
---
+++ src/app/oauth/page.tsx
@@ -0,0 +1,7 @@
+export default function Oauth() {
+  return (
+    <main className="main-container mt-16">
+      // TODO: 여기에서 code query string 떼어서 사용하기 (=인가코드)
+    </main>
+  );
+}
