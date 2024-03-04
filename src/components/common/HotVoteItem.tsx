0b9aa1c18dee5c37070cc8f26918f53f0c69d339
Yujin Cha <youjincha4821@gmail.com>
2024-03-04 13:50:37 +0900

    fix: hot vote item CSS 스타일

--- src/components/common/HotVoteItem.tsx
+++ src/components/common/HotVoteItem.tsx
@@ -10,12 +10,14 @@ interface HotVoteProp {
 export default function HotVoteItem({ data, index }: HotVoteProp) {
   console.table(data);
   return (
-    <div className="relative flex min-h-40 min-w-[33%] flex-col rounded bg-gray-white p-4 sm:min-w-full md:min-w-[50%]">
-      <div className="label">
+    <div className="relative flex min-h-40 min-w-[33%] flex-col justify-between rounded bg-white p-4 shadow-lg sm:min-w-full md:min-w-[50%]">
+      <div className="label-ribbon">
         <span>TOP {index + 1}</span>
       </div>
-      <p className="text-xl font-bold">{data.title}</p>
-      <div>{data.content}</div>
+      <div className="flex flex-col gap-2">
+        <p className="w-11/12 text-xl font-medium">{data.title}</p>
+        <div className="line-clamp-6">{data.content}</div>
+      </div>
       <div className="flex items-center gap-1">
         <div className="mr-1">{data.createdAt}</div>
         <ChatBubbleBottomCenterTextIcon width={20} height={20} />
